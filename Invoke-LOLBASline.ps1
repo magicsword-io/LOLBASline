dc99279c4b2c4c043021554eaba482e18e3e38b6
Jose Enrique Hernandez <josehelps@gmail.com>
2024-03-11 17:26:50 -0400

    Update Invoke-LOLBASline.ps1

--- Invoke-LOLBASline.ps1
+++ Invoke-LOLBASline.ps1
@@ -1,14 +1,46 @@
+<#
+.SYNOPSIS
+LOLBASline - A PowerShell tool for checking the presence and execution status of Living Off The Land Binaries and Scripts (LOLBAS).
+
+.DESCRIPTION
+LOLBASline checks for the existence of specified binaries and attempts to execute commands from the LOLBAS project definitions. It provides insights into which LOLBAS items are present and executable on a Windows system. Use this tool in controlled environments to assess system exposure to threats that use LOLBAS.
+
+.AUTHOR
+Name: Jose E Hernandez
+Organization: MagicSword
+Email: jose@magicsword.io
+
+.NOTES
+Version:        1
+Last Updated:   03/11/2024
+License:        Apache 2.0
+GitHub:         https://github.com/magicsword-io/LOLBASline
+
+.LINK
+LOLBAS Project - https://github.com/LOLBAS-Project/LOLBAS
+#>
+
 param (
     [string]$Path = $null,
     [string]$Output = "results.csv",
     [switch]$Verbose
 )
 
+# Import required module
+Import-Module powershell-yaml -ErrorAction Stop
+
 function Clone-LOLBASRepo {
-    $RepoURL = "https://github.com/LOLBAS-Project/LOLBAS.git"
-    $Destination = "lolbas_repo"
-    Write-Host "Cloning LOLBAS project to $Destination..."
-    git clone $RepoURL $Destination
+    param (
+        [string]$Destination
+    )
+    
+    if (-not (Test-Path $Destination)) {
+        $RepoURL = "https://github.com/LOLBAS-Project/LOLBAS.git"
+        Write-Host "Cloning LOLBAS project to $Destination..."
+        git clone $RepoURL $Destination
+    } else {
+        Write-Host "$Destination already exists. Using existing repository."
+    }
     return "$Destination/yml/OSBinaries"
 }
 
@@ -29,7 +61,7 @@ function Load-YAMLFiles {
     return $YamlObjects
 }
 
-function Check-Binaries {
+function Check-And-Execute-Binaries {
     param (
         [System.Collections.ArrayList]$YamlData,
         [switch]$Verbose
@@ -38,31 +70,41 @@ function Check-Binaries {
     $Results = @()
 
     foreach ($Data in $YamlData) {
-        foreach ($Path in $Data.Full_Path) {
-            $ExecutablePath = $Path.Path
-            $Result = [PSCustomObject]@{
-                Name       = $Data.Name
-                Path       = $ExecutablePath
-                Loaded     = "No"
-                Command    = ""
-                Usecase    = ""
-                Category   = ""
-            }
-
-            if (Test-Path $ExecutablePath) {
-                $Result.Loaded = "Yes"
-                if ($Data.Commands) {
-                    $CommandInfo = $Data.Commands[0]
-                    $Result.Command = $CommandInfo.Command
-                    $Result.Usecase = $CommandInfo.Usecase
-                    $Result.Category = $CommandInfo.Category
+        if ($Data.Commands) {
+            foreach ($CommandInfo in $Data.Commands) {
+                $ExecutableCommand = $CommandInfo.Command
+                try {
+                    $process = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $ExecutableCommand" -PassThru -WindowStyle Hidden
+                    Start-Sleep -Seconds 2 # Give the command a moment to execute; adjust as needed
+                    if ($process.HasExited -eq $false) {
+                        $process.Kill()
+                        $executionResult = "Executed"
+                    } else {
+                        $executionResult = "Failed"
+                    }
+                } catch {
+                    $executionResult = "Error"
                 }
-            }
 
-            $Results += $Result
+                $Result = [PSCustomObject]@{
+                    Name            = $Data.Name
+                    Command         = $ExecutableCommand
+                    ExecutionResult = $executionResult
+                    Description     = $CommandInfo.Description
+                    Usecase         = $CommandInfo.Usecase
+                    Category        = $CommandInfo.Category
+                }
 
-            if ($Verbose) {
-                Write-Host "Checked $ExecutablePath: Loaded = $($Result.Loaded)"
+                $Results += $Result
+
+                if ($Verbose) {
+                    $color = switch ($executionResult) {
+                        "Executed" { "Green" }
+                        "Failed"   { "Red" }
+                        Default    { "Yellow" }
+                    }
+                    Write-Host "$($Data.Name): Command execution result = $($executionResult)" -ForegroundColor $color
+                }
             }
         }
     }
@@ -71,10 +113,10 @@ function Check-Binaries {
 }
 
 if (-not $Path) {
-    $Path = Clone-LOLBASRepo
+    $Path = Clone-LOLBASRepo -Destination "lolbas_repo"
 }
 
 $YamlData = Load-YAMLFiles -DirectoryPath $Path
-$Results = Check-Binaries -YamlData $YamlData -Verbose:$Verbose
+$Results = Check-And-Execute-Binaries -YamlData $YamlData -Verbose:$Verbose
 $Results | Export-Csv -Path $Output -NoTypeInformation
 Write-Host "Results written to $Output"
